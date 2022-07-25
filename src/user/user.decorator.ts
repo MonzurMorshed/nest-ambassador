1ea7eb273193857711308f8512fba2a7a52b5761
MonzurMorshed <monzurmorshedcse@gmail.com>
2022-07-26 01:15:21 +0600

    user decorator for scope.

--- src/user/user.decorator.ts
+++ src/user/user.decorator.ts
@@ -5,6 +5,8 @@ export const User = createParamDecorator(
   async (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const userService = new UserService();
+    const is_ambassador = request.path.toString.indexOf('/api/ambassador') >= 0;
+    const scope = is_ambassador ? 'ambassador' : 'admin';
     return await userService.get('user', {},request.cookies['jwt']);
   },
 );
